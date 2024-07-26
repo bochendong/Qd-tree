3c99c6eab8f5ff02de0f804557309057ceb874ab
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-07-26 07:33:28 -0400

    Update Proprecess.py

--- Code/DataSet/Proprecess.py
+++ Code/DataSet/Proprecess.py
@@ -1,9 +1,11 @@
 import os
 import cv2 as cv
 import numpy as np
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
 from ..QdTree.quadtree import FixedQuadTree
 
-def seqence_image(image_path, to_size = (8, 8, 3), fixed_length = 1024):
+def seqence_image(image_path, to_size=(8, 8, 3), fixed_length=1024):
     img = cv.imread(image_path)
     img = cv.resize(img, (512, 512))
 
@@ -12,18 +14,25 @@ def seqence_image(image_path, to_size = (8, 8, 3), fixed_length = 1024):
 
     edges = cv.Canny(gray_img, 80, 100)
 
-    qdt = FixedQuadTree(domain = edges, fixed_length = fixed_length)
+    qdt = FixedQuadTree(domain=edges, fixed_length=fixed_length)
 
-    seq_img = qdt.serialize(img, size = to_size)
+    seq_img = qdt.serialize(img, size=to_size)
     seq_img = np.asarray(seq_img)
     seq_img = np.reshape(seq_img, [to_size[0], -1, to_size[2]])
 
     return seq_img, qdt
 
-def preprocess_image(root_dir, out_dir, to_size, fixed_length):
+def process_single_image(args):
+    img_path, out_img_path, to_size, fixed_length = args
+    seq_img, _ = seqence_image(img_path, to_size, fixed_length)
+    cv.imwrite(out_img_path, seq_img)
+
+def preprocess_image(root_dir, out_dir, to_size, fixed_length, num_threads=4):
     if not os.path.exists(out_dir):
         print(f"Out dir created at {out_dir}.")
         os.makedirs(out_dir)
+
+    tasks = []
 
     for class_id, class_name in enumerate(os.listdir(root_dir)):
         class_dir = os.path.join(root_dir, class_name)
@@ -36,12 +45,11 @@ def preprocess_image(root_dir, out_dir, to_size, fixed_length):
         if os.path.isdir(class_dir):
             for img_name in os.listdir(class_dir):
                 img_path = os.path.join(class_dir, img_name)
-
-                seq_img, _ = seqence_image(img_path, to_size, fixed_length)
                 out_img_path = os.path.join(out_class_dir, img_name)
-                cv.imwrite(out_img_path, seq_img)
-        
+                tasks.append((img_path, out_img_path, to_size, fixed_length))
 
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        list(tqdm(executor.map(process_single_image, tasks), total=len(tasks)))
 
 if __name__ == '__main__':
     to_size = (8, 8, 3)
@@ -50,6 +58,4 @@ if __name__ == '__main__':
     root_dir = "/lustre/orion/bif146/world-shared/enzhi/imagenet2012/train/"
     out_dir = f"/lustre/orion/bif146/world-shared/enzhi/qdt_imagenet/preprocess_data/{fixed_length}_{to_size}/train/"
 
-    preprocess_image(root_dir, out_dir)
-
-
+    preprocess_image(root_dir, out_dir, to_size, fixed_length)
