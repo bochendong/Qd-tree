2aef23b891f93dd341fd7a7202d94e70eb743b8a
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-07-26 07:00:55 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -15,8 +15,8 @@ fixed_length = 1024
 root_dir = "/lustre/orion/bif146/world-shared/enzhi/imagenet2012/train/"
 preporcess_dir = f"/lustre/orion/bif146/world-shared/enzhi/qdt_imagenet/preprocess_data/{fixed_length}_{to_size}/train/"
 
-# preprocess_image(root_dir, preporcess_dir, to_size = to_size, fixed_length = fixed_length)
-
+preprocess_image(root_dir, preporcess_dir, to_size = to_size, fixed_length = fixed_length)
+'''
 transform = transforms.Compose([
     transforms.ToPILImage(),
     transforms.ToTensor(),
@@ -28,7 +28,7 @@ dataloader = DataLoader(dataset, batch_size=32, shuffle=True, num_workers=4)
 
 for i, data in enumerate(dataloader):
     print(data[0].size()) #torch.Size([32, 3, 8, 8192])
-    print(data[1].size()) # torch.Size([32])
+    print(data[1].size()) # torch.Size([32])'''
 '''
 model = get_vit_model('vit_base_patch16_224', len(dataset.classes))    
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
