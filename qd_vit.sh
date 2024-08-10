22fcff5a02e4e9d7ffaa725b38ca53ec3788233c
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-08-10 00:39:39 -0400

    Update qd_vit.sh

--- qd_vit.sh
+++ qd_vit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -A bif146
 #SBATCH -o qdt_vit_196_128.o%J
-#SBATCH -t 02:00:00
-#SBATCH -N 8
+#SBATCH -t 01:00:00
+#SBATCH -N 1
 #SBATCH -p batch
 #SBATCH --mail-user=dongbochen1218@icloud.com
 #SBATCH --mail-type=END
@@ -17,5 +17,5 @@ module load PrgEnv-gnu
 module load gcc/12.2.0
 module load rocm/5.7.0
 # exec
-srun -N 8 -n 64 --ntasks-per-node 8 /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python Main.py --batch_size 256
+srun -N 1 -n 2 --ntasks-per-node 2 /lustre/orion/bif146/world-shared/gvit/env/miniconda3/envs/gvit/bin/python Main.py --batch_size 256
 
